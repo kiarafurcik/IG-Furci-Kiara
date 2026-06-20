e101f38ff231dbcdf0bd82665ab589445c2a5848
kiarafurcik <kiarafurcik@gmail.com>
2026-06-20 19:35:21 -0300

    practica 16 hecha

---
+++ PRACTICA-16/js/script.js
@@ -0,0 +1,17 @@
+let nombre = "Kiara"
+let apellido = "Furci"
+let nombreCompleto = nombre + " " + apellido
+let edad = "18"
+const universidad = "Universidad Nacional de las Artes"
+console.log(nombreCompleto)
+console.log(edad)
+console.log(universidad)
+
+nombre = "Constanza"
+apellido = "Kupelian"
+nombreCompleto = nombre + " " + apellido
+edad = "87"
+universidad = "UNA"
+console.log(nombreCompleto)
+console.log(edad)
+console.log(universidad)
