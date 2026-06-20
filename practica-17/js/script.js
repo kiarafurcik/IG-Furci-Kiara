a52ee0b40e29a32727d8d3ac423929a736c16704
kiarafurcik <kiarafurcik@gmail.com>
2026-06-20 20:46:36 -0300

    practica 17 realizada

---
+++ practica-17/js/script.js
@@ -0,0 +1,6 @@
+let precioProducto = 50000
+let descuento = 20
+let resultado = precioProducto - (precioProducto * descuento / 100)
+console.log("Precio sin descuento: $" + precioProducto)
+console.log("El descuento es:" + descuento)
+console.log("Precio con descuento es:" + resultado)
