421d1084a313837bd0cb8fa665aacc77b31ea050
kiarafurcik <kiarafurcik@gmail.com>
2026-06-21 03:10:55 -0300

    practica 18 terminada

---
+++ practica-18/js/script.js
@@ -0,0 +1,19 @@
+alert ("Al aceptar cambia todo")
+
+let titulo = document.querySelector("h1")
+titulo.style.color = "#19288c"
+titulo.style.fontSize = "18px"
+titulo.innerText = "Que hiciste?"
+
+let parrafos = document.querySelector(".uno")
+parrafos.style.color = "#8c196f"
+parrafos.style.fontSize = "24px"
+parrafos.innerText = "No me gustó que hagas eso"
+
+let parrafos2 = document.querySelector(".dos")
+parrafos2.style.color = "#800000"
+parrafos2.style.fontSize = "36px"
+parrafos2.innerText = "ME ENOJEEEEEE"
+
+let fondo = document.querySelector("body")
+fondo.style.backgroundColor = "#000000"
